32ad363f55d84f2560296c040f790a8340ae3cb9
Amade Omar Ismael <amadeomarismael@hotmail.com>
2016-08-26 10:26:01 +0200

    project converter: commit após escrever o segundo teste (que quebrou por ainda estar aceitando qualquer entrada, não há validação)

--- src/test/java/mz/com/cedsif/converter/models/ConverterTest.java
+++ src/test/java/mz/com/cedsif/converter/models/ConverterTest.java
@@ -4,22 +4,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
 import static org.hamcrest.Matchers.*;
 
 public class ConverterTest {
 
 	private Converter converter;
-	
+
 	@Before
 	public void setUp() {
-		converter= new Converter();
+		converter = new Converter();
+	}
+
+	@Test
+	public void deveConverterParaNumericoQualquerRomanoEntre1A3999() {
+		converter.setValorDesejado("MMM");
+
+		assertThat(converter.converterArabicoRomano(), equalTo(2000));
 	}
 	
-	@Test
-	public void deveConverterNumerosMaioresQue1() {
-		converter.setValorDesejado("MMM");
-		
-		assertThat(converter.converterArabicoRomano(), equalTo(2000));
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void naoDeveAceitarQualquerTipoDeEntrada() {
+		converter.setValorDesejado("cedsif matrix caelum amade hélio nelúcia");
 	}
 }
