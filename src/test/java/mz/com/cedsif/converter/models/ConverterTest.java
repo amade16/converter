a58e238af71b362acc01767a4789c903557b144e
Amade Omar Ismael <amadeomarismael@hotmail.com>
2016-08-26 03:50:50 +0200

    project converter: commit após escrever o primeiro teste (que quebrou por ainda não existir o código fonte)

---
+++ src/test/java/mz/com/cedsif/converter/models/ConverterTest.java
@@ -0,0 +1,25 @@
+package mz.com.cedsif.converter.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.*;
+
+public class ConverterTest {
+
+	private Converter converter;
+	
+	@Before
+	public void setUp() {
+		converter= new Converter();
+	}
+	
+	@Test
+	public void deveConverterNumerosMaioresQue1() {
+		converter.setValorDesejado(2);
+		
+		assertThat(converter.converter_arabico_romano(), equalTo("II"));
+	}
+}
