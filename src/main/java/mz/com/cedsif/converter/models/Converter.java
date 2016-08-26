d7468b40b18e6469cb580e820690e67aa52de8fb
Amade Omar Ismael <amadeomarismael@hotmail.com>
2016-08-26 23:46:38 +0200

    project converter: commit da primeira versão executável

--- src/main/java/mz/com/cedsif/converter/models/Converter.java
+++ src/main/java/mz/com/cedsif/converter/models/Converter.java
@@ -6,7 +6,8 @@ public class Converter {
 
 	private String valor_desejado;
 	private int valor_convertido;
-	private HashMap<Character, Integer> dicionario;
+
+	private static HashMap<Character, Integer> caracteres_validos;
 
 	public Converter() {
 		inicializarDicionario();
@@ -29,44 +30,106 @@ public class Converter {
 	}
 
 	public int converterArabicoRomano() {
-		return resultadoDaConversaoArabicoRomano(this.valor_desejado);
+
+		validarCaracteresInvalidos(this.valor_desejado);
+
+		validarRepeticao(this.valor_desejado);
+
+		return resultadoConversao(this.valor_desejado);
 	}
 
-	private int resultadoDaConversaoArabicoRomano(String valor_desejado) {
-		int valor_convertido=0;
-		int i=0;
-		for (i=0; i < valor_desejado.length()-1; i++) {
+	private int resultadoConversao(String valor_desejado) {
+
+		int valor_convertido = 0;
+
+		for (int i = 0; i <= valor_desejado.length() - 1; i++) {
 			
-			caracteresLegais(valor_desejado.charAt(i),valor_desejado.charAt(i+1));
+			Character caracter_actual = valor_desejado.charAt(i);
 			
-			if (dicionario.get(valor_desejado.charAt(i)) >= dicionario.get(valor_desejado.charAt(i+1))) {
-                valor_convertido += dicionario.get(valor_desejado.charAt(i));
-            }
-			else {
-				valor_convertido -= dicionario.get(valor_desejado.charAt(i));
-            }      
+			
+			if(i==valor_desejado.length() - 1) {
+				valor_convertido += mapearRomanoNumerico(caracter_actual);
+			}
+			else {Character proximo_caracter = valor_desejado.charAt(i + 1);
+				if (mapearRomanoNumerico(caracter_actual) >= mapearRomanoNumerico(proximo_caracter)) {
+					valor_convertido += mapearRomanoNumerico(caracter_actual);
+				} else {
+					valor_convertido -= mapearRomanoNumerico(caracter_actual);
+				}
+			}
+			
 		}
-		
-		valor_convertido += dicionario.get(valor_desejado.charAt(i));
 		
 		return valor_convertido;
 	}
-	
-	private void caracteresLegais (char valor_actual, char proximo_valor) {
-		if(dicionario.get(valor_actual)==null || dicionario.get(proximo_valor)==null) {
-			throw new IllegalArgumentException("Erro! O valor introduzido não contém correspondentes romanos válidos!");
+
+	private int mapearRomanoNumerico(Character valor_a_mapear) {
+		return caracteres_validos.get(valor_a_mapear);
+	}
+
+	private void validarCaracteresInvalidos(String valor_desejado) {
+		
+		for (int i = 0; i < valor_desejado.length() - 1; i++) {
+			
+			if (caracteres_validos.get(valor_desejado.charAt(i)) == null) {
+				throw new IllegalArgumentException("Erro! Caracteres inválidos!");
+			}
 		}
 	}
 
-	private void inicializarDicionario() {
-		dicionario = new HashMap<Character, Integer>();
-		dicionario.put('I', 1);
-		dicionario.put('V', 5);
-		dicionario.put('X', 10);
-		dicionario.put('L', 50);
-		dicionario.put('C', 100);
-		dicionario.put('D', 500);
-		dicionario.put('M', 1000);
+	private void validarRepeticao(String valor_desejado) {
+		char[] caracteres_no_valor_desejado = valor_desejado.toCharArray();
+
+		for (int i = 0; i < caracteres_no_valor_desejado.length; i++) {
+			if (contarOcorrencias(caracteres_no_valor_desejado,caracteres_no_valor_desejado[i])) {
+				throw new IllegalArgumentException("Erro! Repetições inválidas!");
+			}
+		}
 	}
 
+	public boolean contarOcorrencias(char[] valor_desejado_caracteres, char caracter_actual) {
+		String caracteres_que_podem_ocorrer_3_vez = "IXCM";
+
+		String caracteres_que_podem_ocorrer_1_vez = "VLD";
+
+		int contador_ocorrencias = 0;
+
+		// se existir possso em seguida contar a ocorrencia
+		if (caracteres_que_podem_ocorrer_3_vez.indexOf(caracter_actual) > -1) {
+			for (int i = 0; i < valor_desejado_caracteres.length; i++) {
+				if (valor_desejado_caracteres[i] == caracter_actual) {
+					contador_ocorrencias++;
+				}
+
+				if (contador_ocorrencias > 3) {
+					return true;
+				}
+			}
+		} else {
+			if (caracteres_que_podem_ocorrer_1_vez.indexOf(caracter_actual) > -1) {
+				for (int i = 0; i < valor_desejado_caracteres.length; i++) {
+					if (valor_desejado_caracteres[i] == caracter_actual) {
+						contador_ocorrencias++;
+					}
+
+					if (contador_ocorrencias > 1) {
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private static void inicializarDicionario() {
+		caracteres_validos = new HashMap<Character, Integer>();
+		caracteres_validos.put('I', 1);
+		caracteres_validos.put('V', 5);
+		caracteres_validos.put('X', 10);
+		caracteres_validos.put('L', 50);
+		caracteres_validos.put('C', 100);
+		caracteres_validos.put('D', 500);
+		caracteres_validos.put('M', 1000);
+	}
 }
--- src/test/java/mz/com/cedsif/converter/models/ConverterTest.java
+++ src/test/java/mz/com/cedsif/converter/models/ConverterTest.java
@@ -17,16 +17,40 @@ public class ConverterTest {
 
 	@Test
 	public void deveConverterParaNumericoQualquerRomanoEntre1A3999() {
-		converter.setValorDesejado("MMM");
-
-		assertThat(converter.converterArabicoRomano(), equalTo(3000));
+		converter.setValorDesejado("XXXVII");
+		assertThat(converter.converterArabicoRomano(), equalTo(37));
+		
+		converter.setValorDesejado("XIX");
+		assertThat(converter.converterArabicoRomano(), equalTo(19));
+		
+		
+		converter.setValorDesejado("LIV");
+		assertThat(converter.converterArabicoRomano(), equalTo(54));
+		
+		
+		converter.setValorDesejado("CXXIX");
+		assertThat(converter.converterArabicoRomano(), equalTo(129));
+		
+		converter.setValorDesejado("M");
+		assertThat(converter.converterArabicoRomano(), equalTo(1000));		
 	}
 	
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void naoDeveAceitarQualquerTipoDeEntrada() {
-		converter.setValorDesejado("cedsif matrix caelum amade hélio nelúcia 2016");
-		
+		converter.setValorDesejado("cedsif matrix caelum amade hélio nelúcia silvano 2016");		
+		converter.converterArabicoRomano();
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void naoDeveConterVLDMaisQue1Vez() {
+		converter.setValorDesejado("VV");		
+		converter.converterArabicoRomano();
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void naoDeveConterIXCMMaisQue3Vezes() {
+		converter.setValorDesejado("MCMLXXXIVX");		
 		converter.converterArabicoRomano();
 	}
 }
